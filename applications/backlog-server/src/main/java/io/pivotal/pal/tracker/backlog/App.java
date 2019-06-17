5f2b207e28c16c9ee6c45d680496fd706794abd5
leonardopordeus <leonardopordeus@gmail.com>
2019-06-17 14:20:23 -0600

    Circuit Breaker

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -1,8 +1,10 @@
 package io.pivotal.pal.tracker.backlog;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +12,7 @@ import org.springframework.web.client.RestOperations;
 
 import java.util.TimeZone;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
